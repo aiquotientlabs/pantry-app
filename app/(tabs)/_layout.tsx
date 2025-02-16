b7d64114d4a679ec5f06db93adfabbbd6db81832
Shawn N <shawnneill24@gmail.com>
2025-02-16 15:04:49 -0700

    Project Setup with Tab Navigation
    -Initialized React Native app with Expo
    -Tab navigation (Home, Pantry, Add Item)
    -Basic Light / Dark theme

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,51 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Platform } from 'react-native';
+
+import { HapticTab } from '@/components/HapticTab';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors['orange'].tabIconSelected,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: 'absolute',
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="addItem"
+        options={{
+          title: 'Add Item',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="cart.fill.badge.plus" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="pantry"
+        options={{
+          title: 'Pantry',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="list.bullet" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
