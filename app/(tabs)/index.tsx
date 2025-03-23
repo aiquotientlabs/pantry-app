770e95ad175e9f0ee9c2115ddb083b1f2e559cf7
Shawn N <79764274+ShawnN24@users.noreply.github.com>
2025-03-23 04:27:36 -0700

    Basic Dashboard UI
    -Basic Scroll View and Button for other additions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,29 @@
-import { Text, View } from "react-native";
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { ItemContainer } from "@/components/ItemContainer";
+import { TabContainer } from "@/components/TabContainer";
+import { ThemedButton } from "@/components/ThemedButton";
+import { ThemedIcon } from "@/components/ThemedIcon";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { ScrollView } from "react-native";
+
 
 export default function Index() {
-  const colorScheme = useColorScheme();
-  
   return (
-    <ThemedView 
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <ThemedText>HOME DASHBOARD</ThemedText>
-    </ThemedView>
+    <TabContainer>
+      <ThemedView style={{flex: 1}}>
+        <ThemedButton style={{flex: 1}} type="grey" onPress={() => {}}>
+          <ThemedIcon size={35} name="plus" type='grey' />
+        </ThemedButton>
+      </ThemedView>
+      <ThemedView style={{flex: 1}}>
+        <ThemedText type="title" style={{paddingBottom: 10}}>Expiring Soon:</ThemedText>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <ThemedView style={{gap: 10}}>
+            <ItemContainer type="grey" name="Apple" quantity="5" category="Fruit" expiration="03-15-2025" />
+            <ItemContainer type="grey" name="B" quantity="\" category="\" expiration="\" /><ItemContainer type="grey" name="C" quantity="\" category="\" expiration="\" /><ItemContainer type="grey" name="D" quantity="\" category="\" expiration="\" /><ItemContainer type="grey" name="E" quantity="\" category="\" expiration="\" /><ItemContainer type="grey" name="F" quantity="\" category="\" expiration="\" /><ItemContainer type="grey" name="G" quantity="\" category="\" expiration="\" /><ItemContainer type="grey" name="H" quantity="\" category="\" expiration="\" />
+          </ThemedView>
+        </ScrollView>
+      </ThemedView>
+    </TabContainer>
   );
 }
